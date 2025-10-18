e3f482748b855deba9e26efe373b19984e4e2d83
Gagan swamy ks <66283267+gaganswamyks7771@users.noreply.github.com>
2025-10-18 08:09:48 +0530

    Update app.py

--- OSC/app.py
+++ OSC/app.py
@@ -138,5 +138,5 @@ def _download(path: str, name: str):
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
     # app.run()
